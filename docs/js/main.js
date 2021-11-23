fb955b32a4693985281c170f6a8913f991dd4b9c
kalebp731 <83477147+kalebp731@users.noreply.github.com>
2021-11-23 14:23:03 -0500

    adding rps code to js

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,32 +1,76 @@
-document.querySelector('#yell').addEventListener('click', run)
-
-function run() {
-  const fName = document.querySelector('#firstName').value
-  const fMidName = document.querySelector('#firstMiddle').value
-  const lMidName = document.querySelector('#lastMiddle').value
-  const lName = document.querySelector('#lastName').value
-
-  // document.querySelector('#placeToYell').innerText = fName + ' ' + fMidName + ' ' + ' ' + lMidName + ' ' + lName
-
-  //Add what you should be doing - conditionals go here
-
-  document.querySelector('#placeToYell').innerText = `${fName} ${fMidName} ${lMidName} ${lName}`
-}
-
-// const synth = window.speechSynthesis;
-// document.querySelector('#yell').addEventListener('click', run)
-//
-// function run() {
-//   const fName = document.querySelector('#firstName').value
-//   const fMidName = document.querySelector('#firstMiddle').value
-//   const lMidName = document.querySelector('#lastMiddle').value
-//   const lName = document.querySelector('#lastName').value
-//
-//   const yellText =  `${fName} ${fMidName} ${lMidName} ${lName}`
-//
-//   document.querySelector('#placeToYell').innerText = yellText
-//
-//   let yellThis = new SpeechSynthesisUtterance(yellText);
-//
-//   synth.speak(yellThis);
-// }
+let userInput = document.querySelector('#userHand').value
+userInput.toLowerCase()
+const getUserChoice = (userInput) => {
+  if (
+    userInput === 'rock' ||
+    userInput === "paper" ||
+    userInput === "scissors" ||
+    userInput === "bomb"
+  ) {
+    return userInput;
+  } else {
+    alert("error");
+  }
+};
+const getComputerChoice = () => {
+  let randomNumber = Math.floor(Math.random() * 3);
+  switch (randomNumber) {
+    case 0:
+      return "rock";
+    case 1:
+      return "paper";
+    case 2:
+      return "scissors";
+  }
+};
+const determineWinner = (userChoice, computerChoice) => {
+  if (userChoice === "bomb") {
+    return "you win!";
+  }
+  if (userChoice === computerChoice) {
+    return "Tie";
+  }
+  if (userChoice === "rock") {
+    if (computerChoice === "paper") {
+      return "The computer won!";
+    } else {
+      return "You won!";
+    }
+  }
+  if (userChoice === "paper") {
+    if (computerChoice === "scissors") {
+      return "The computer won!";
+    } else {
+      return "You won!";
+    }
+  }
+  if (userChoice === "paper") {
+    if (computerChoice === "rock") {
+      return "computer wins!";
+    } else {
+      return "you won!";
+    }
+  }
+  if (userChoice === "scissors") {
+    if (computerChoice === "rock") {
+      return "The computer won!";
+    } else {
+      return "You won!";
+    }
+  }
+  if (userChoice === "scissors") {
+    if (computerChoice === "paper") {
+      return "computer wins!";
+    } else {
+      return "you won!";
+    }
+  }
+};
+const playGame = () => {
+  let userChoice = getUserChoice();
+  let computerChoice = getComputerChoice();
+  console.log("you threw: " + userChoice);
+  console.log("computer threw: " + computerChoice);
+  console.log(determineWinner(userChoice, computerChoice));
+};
+document.querySelector('#throw').addEventListener('click', playGame)
